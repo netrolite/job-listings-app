2c983c65d15b482af16ebb9f1f7ab0fbb8fb0bb9
Matvey <morozovmatvej75@gmail.com>
2023-01-30 20:04:26 +0300

    add custom errors

---
+++ server/errors/NotFoundErr.js
@@ -0,0 +1,8 @@
+class NotFoundErr extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = 404;
+    }
+}
+
+module.exports = NotFoundErr;
---
+++ server/errors/index.js
@@ -0,0 +1,9 @@
+const BadRequestErr = require("./BadRequestErr");
+const NotFoundErr = require("./NotFoundErr");
+const UnauthorizedErr = require("./UnauthorizedErr");
+
+module.exports = {
+    BadRequestErr,
+    NotFoundErr,
+    UnauthorizedErr
+}
